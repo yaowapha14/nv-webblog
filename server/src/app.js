530b74db8ee7455f8cb706cc00abcbdbbc8293c4
yaowapha14 <yaowaphaf61@nu.ac.th>
2020-07-16 23:39:44 +0700

    foloder perpared

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,5 @@
-let exprss = require('express')
-const app = exprss() 
+let express = require('express')
+const app = express()
 let bodyParser = require('body-parser')
 const {sequelize} = require('./models')
 const config = require('./config/config')
@@ -8,6 +8,13 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
 require('./routes')(app)
+
+let port = process.env.PORT || config.port
+sequelize.sync({force: false}).then(() => {
+    app.listen(port, function () {
+        console.log('Server running on ' + port)
+    })
+})
 
 app.get('/status', function (req, res){
     res.send('Hello nodejs server')
@@ -21,34 +28,22 @@ app.get('/hello/:person', function (req,res) {
 // get user by id
 app.get('/user/:userId', function (req, res) {
     res.send('ดูข้อมูลผู้ใช้งาน: '+ req.params.userId)
-})
+    })
 // get all user
 app.get('/users', function (req, res) {
-    res.send('เรียกข้อมูล ผู้ใช้งานทั้งหมด')
+    res.send('เรียกข้อมูลผู้ใช้งานทั้งหมด')
 })
-
-app.listen(port, function () {
-    console.log('server running on ' + port)
-})
-
 // create user
 app.post('/user/', function (req, res) {
     res.send('ทำการสร้างผู้ใช้งาน: ' + JSON.stringify(req.body))
 })
 // edit user
 app.put('/user/:userId', function (req, res) {
-    res.send('ทำการแก้ไขผู้ใช้งาน: ' + req.params.userId + ' : ' +
+    res.send('ทำการแก้ไขผุ้ใช้งาน: ' + req.params.userId + ' : ' +
     JSON.stringify(req.body))
 })
 // delete user
 app.delete('/user/:userId', function (req, res) {
     res.send('ทำการลบผู้ใช้งาน: ' + req.params.userId + ' : ' +
     JSON.stringify(req.body))
-})
-
-let port = process.env.PORT || config.port
-sequelize.sync({force: false}).then(() => {
-    app.listen(port, function () {
-        console.log('Server running on ' + port)
-    })
 })
