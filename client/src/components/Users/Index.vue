5455426668d0578e2ff445313f409c231cea04c8
yaowapha14 <yaowaphaf61@nu.ac.th>
2020-07-22 21:18:41 +0700

    folder perpared

---
+++ client/src/components/Users/Index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1>Get All Users</h1>
+    </div>
+</template>
+<script>
+    export default {
+    }
+</script>
+<style scoped>
+</style>
---
+++ client/src/components/Users/ShowUser.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1>Get User By Id</h1>
+    </div>
+</template>
+<script>
+    export default {
+    }
+</script>
+<style scoped>
+</style>
